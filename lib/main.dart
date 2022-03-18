dac33529e931dd7775789674358eb7ca4fceb888
Alex Ichenskiy <aichenskiy@gmail.com>
2022-03-18 15:55:24 +0100

    init: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Cocktail',
+      home: Main(),
+    )
+  );
+}
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Flutter Cocktail",
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 1;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    HomeWidget(),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Cocktail',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Cocktail'),
+          backgroundColor: Colors.pink.shade500,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.pink.shade500,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+              ),
+              label: 'Favorite',
+              backgroundColor: Colors.white,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_rounded,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.library_books_sharp,
+              ),
+              label: 'Categories',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Colors.cyan.shade300,
+          selectedFontSize: 18,
+          unselectedFontSize: 16,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container (
+      child: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.pink.shade100,
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.1,
+            child: Text("Hello"),
+          ),
+          Container(
+            color: Colors.pink.shade100,
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.667,
+            child: Text("Hello"),
+          ),
+        ],
+      ),
+    );
+  }
+}
